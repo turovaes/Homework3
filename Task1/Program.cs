c720adaaac4c33ce60a8c41deffc82834e554858
turovaes <turovaes@gmail.com>
2023-02-02 21:56:56 +0100

    Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+
+int a = number;
+int reverse = 0;
+int count = 0;
+
+
+while (number > 0)
+{
+    int modulo = number % 10;
+    reverse = reverse * 10 + modulo;
+    number = number / 10;
+    count++;
+}
+
+if (a == reverse && count == 5)
+{
+    Console.WriteLine("Это палиндром");
+}
+else if (count != 5)
+{
+    Console.WriteLine("Вы ввели не пятизначное число. Попробуйте еще раз");
+}
+else Console.WriteLine("Извините, но это не палиндром");
+
+
+
+
