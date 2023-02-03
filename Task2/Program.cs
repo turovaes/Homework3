cffd8018083c36391835366a4cafa820d9d2b761
turovaes <turovaes@gmail.com>
2023-02-03 08:45:46 +0100

    Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите координаты первой точки А(x1,y1,z1)");
+
+int x1 = int.Parse(Console.ReadLine());
+int y1 = int.Parse(Console.ReadLine());
+int z1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите координаты второй точки B(x2,y2,y3)");
+
+int x2 = int.Parse(Console.ReadLine());
+int y2 = int.Parse(Console.ReadLine());
+int z2 = int.Parse(Console.ReadLine());
+
+double result = Math.Sqrt(Math.Pow((x2 - x1),2) + Math.Pow((y2 - y1),2) + Math.Pow((z2 - z1),2));
+
+
+Console.WriteLine($"Длина отрезка AB  {result}");
