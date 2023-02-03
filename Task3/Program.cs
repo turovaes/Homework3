adc0bd66ca84fb0df2f70f788f294f2a401a8bc4
turovaes <turovaes@gmail.com>
2023-02-03 08:59:12 +0100

    Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите число N ");
+int n = int.Parse(Console.ReadLine());
+
+int count = 1;
+
+while (count <= n)
+{
+    double square = Math.Pow((count),3);
+    Console.Write($"{square} ");
+
+    count++;
+    
+}
+
